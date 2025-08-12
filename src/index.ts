d94c3bb22795b995dbd31df9a64737a79c6c0545
TelMayank7 <mayank.panwar@telepathyinfotech.com>
2025-08-12 18:15:08 +0530

    basic setup

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./configs/connectDB";
+import cookieParser from "cookie-parser";
+import configureCors from "./configs/cors.config";
+import { errorHandler } from "./middlewares/errorHandler";
+import limiter from "./middlewares/rateLimit";
+
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(configureCors());
+app.use(limiter);
+
+// Routes
+
+// Global Error Handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
